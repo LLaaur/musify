b6397cab034fc4dd537ae8d47da1f1e8cbb6b8e7
Laur <laur_laurentiu2013@yahoo.com>
2023-01-17 15:41:20 +0200

    fix scroll down bug on mobile devices

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,42 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from "swiper";
+
+import 'swiper/css';
+import 'swiper/css/free-mode'
+
+import PlayPause from "./PlayPause";
+import { playPause } from "../redux/features/playerSlice";
+import { setActiveSong } from "../redux/features/playerSlice";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+const TopPlay = () => {
+
+  const dispatch = useDispatch();
+  const {activeSong, isPlaying} = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+
+  const divRef = useRef(null);
+
+
+  // for fixing the scroll down bug on mobile devices
+  useEffect(() => {
+    divRef.current.scrollIntoView({behavior: 'smooth'});
+  });
+
+
+  return (
+
+    <div ref={divRef}>
+
+
+
+    </div>
+
+  )
+
+}
 
 export default TopPlay;
