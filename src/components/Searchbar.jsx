39285767d00cabb8ed9a5e843efae5f65dca3a6b
Laur <laur_laurentiu2013@yahoo.com>
2023-01-22 15:00:17 +0200

    create search endpoint and functionality

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,21 +2,35 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
 
-const Searchbar = () => (
-  <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+const Searchbar = () => {
 
-    <label htmlFor="search-field" className="sr-only">
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
 
-      Search all songs
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    </label>
-    
-    <div className="flex flex-row justify-start items-center">
-      <FiSearch className="w-5 h-5 ml-4"/>
-      <input className="flex-1 bg-transparent border-none outline-none placeholder-gray-400 text-base text-white p-4"  type="search" name="search-field" autoComplete="off" id="search-field" placeholder="Search..." value='' onChange={() => {}} />
-          </div>
+    navigate(`/search/${searchTerm}`);
+  };
+ 
+  return (
 
-  </form>
-);
+    <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600" onSubmit={handleSubmit}>
+
+      <label htmlFor="search-field" className="sr-only">
+
+        Search all songs
+
+      </label>
+
+      <div className="flex flex-row justify-start items-center">
+        <FiSearch className="w-5 h-5 ml-4" />
+        <input className="flex-1 bg-transparent border-none outline-none placeholder-gray-400 text-base text-white p-4" type="search" name="search-field" autoComplete="off" id="search-field" placeholder="Search..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+      </div>
+
+    </form>
+
+  )
+};
 
 export default Searchbar;
